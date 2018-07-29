fa4f03b74dd0c2039da479567e9c4954faf3bc4c
mauricio <mauriciojunio@gmail.com>
2018-07-29 01:50:53 -0300

    aprendendo a lidar com conversoes de data

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -10,13 +10,14 @@ class NegociacaoController {
     adiciona(event) {
         event.preventDefault();
 
-        let data = new Date(
-            ...this._inputData.value
-            .split('-')
-            .map((item, indice) => item - indice % 2)
+        let negociacao = new Negociacao(
+            DateConverter.paraData(this._inputData.value),
+            this._inputQuantidade.value, 
+            this._inputValor.value
         );
-
-        let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
         console.log(negociacao);
+
+        let diaMesAno = DateConverter.paraTexto(negociacao.data);
+        console.log(diaMesAno); 
     }
 }
